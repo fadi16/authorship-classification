939f1fac821a59dbdaa10ad1a26439244c77f64e
Fadi Arafeh <fadi.arafeh@student.manchester.ac.uk>
2022-05-08 11:06:06 +0000

    removed bs code

--- cross_encoder.py
+++ cross_encoder.py
@@ -4,6 +4,7 @@ import random
 import sys
 
 import numpy as np
+from sklearn.metrics.pairwise import cosine_distances
 
 from blog_dataset import *
 from model_params import *
@@ -67,23 +68,63 @@ def map_chunks(texts1, texts2):
         zip(texts1, texts2[:len(texts1)]))
 
 
+def test_AV(model, threshold, test_pairs, test_labels):
+    print("Testing Cross Encoder Accuracy on Authorship Validation")
+    pred_scores = model.predict(test_pairs, convert_to_numpy=True, show_progress_bar=True)
+    predicted_labels = [1 if s > threshold else 0 for s in pred_scores]
+    accuracy = metrics.accuracy_score(test_labels, predicted_labels)
+    predictions_df = pd.DataFrame({
+        "predicted_labels": predicted_labels,
+        "actual_labels": test_labels
+    })
+    predictions_df.to_csv(f"test_cross_encoder_AV_eval_authors{len(set(test_labels))}.csv")
+    print(f"Test Accuracy = {accuracy}")
+
+
+def classify_with_embeddings(model, threshold, train_embeddings, train_samples, train_labels, test_embeddings,
+                             test_samples, test_labels, top_k):
+    cos_dists = []
+    for i in range(len(test_embeddings)):
+        val_embedding = [test_embeddings[i]]
+        cos_dists.append(cosine_distances(val_embedding, train_embeddings))
+
+    predicted_labels = []
+
+    for i in range(len(test_embeddings)):
+        candidate_labels = []
+        cos_dist = cos_dists[i]
+        sorted_indicies = np.argsort(cos_dist)[0][:top_k]
+        for topk_index in sorted_indicies:
+            prediction = model.predict([[test_samples[i], train_samples[topk_index]]], convert_to_numpy=True,
+                                       show_progress_bar=True)
+            if prediction > threshold:
+                candidate_label = train_labels[topk_index]
+                candidate_labels.append(candidate_label)
+
+        # now we choose the label with the highest count
+        voted_label = max(set(candidate_labels), key=candidate_labels.count) if len(candidate_labels) != 0 else -1
+        predicted_labels.append(voted_label)
+
+    accuracy = metrics.accuracy_score(test_labels, predicted_labels)
+    predictions_df = pd.DataFrame({
+        "predicted_labels": predicted_labels,
+        "actual_labels": test_labels
+    })
+    predictions_df.to_csv(f"test_e2e_classification_authors{len(set(test_labels))}.csv")
+    print(f"E2E bi-encoder + cross-encoder (k={top_k}) Test Accuracy = {accuracy}")
+
+
 def train(params):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
-
-    # todo
-    params[NO_AUTHORS] = 2
 
     ######## defining the model
     model = CrossEncoder(params[CHECKPOINT], device=device, num_labels=1)
     #########################
     # train, val and test splits
-    train_df, val_df, test_df = get_datasets_for_n_authors_AA(n=params[NO_AUTHORS],
-                                                              val_size=0.1,
-                                                              test_size=0.2,
-                                                              seed=params[SEED])
-    train_df = train_df[:20]
-    val_df = val_df[:10]
+    train_df, val_df, test_df = get_datasets_for_n_authors(n=params[NO_AUTHORS],
+                                                           val_size=0.1,
+                                                           test_size=0.2,
+                                                           seed=params[SEED])
 
     # positive label is 1, negative label is 0
     train_pos, train_neg = create_pos_neg_pairs(train_df)
@@ -126,10 +167,27 @@ def train(params):
 
 
 if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     seed_for_reproducability()
     params = model_paramsAV1
 
+    params[TRAIN_BATCH_SIZE] = 16
+    params[VALID_BATCH_SIZE] = 16
+
     model = train(params)
 
+    # train, val and test splits
+    train_df, val_df, test_df = get_datasets_for_n_authors(n=params[NO_AUTHORS],
+                                                           val_size=0.1,
+                                                           test_size=0.2,
+                                                           seed=params[SEED])
 
+    test_pos, test_neg = create_pos_neg_pairs(test_df)
 
+    test_samples = [[test_pos[i][0], test_pos[i][1]] for i in range(len(test_pos))]
+    test_labels = [1 for _ in range(len(test_pos))]
+    test_samples.extend([[test_neg[i][0], test_neg[i][1]] for i in range(len(test_neg))])
+    test_labels.extend([0 for _ in range(len(test_neg))])
+
+    test_AV(model=model, threshold=0.5, test_pairs=test_samples, test_labels=test_labels)
